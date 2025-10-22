9cd07648d19fb6b4d5b9bb152c73f51202ff62a2
NB-CyberSec-GSU <nbarro1@gsu.edu>
2025-10-22 15:58:37 -0400

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [33.75104, -84.3841536],
+            center: [33.75, -84.38],
             container: 'map',
             map: map,
             zoom: 12
@@ -120,6 +120,7 @@ WildRydes.map = WildRydes.map || {};
         };
     });
 }(jQuery));
+
 
 
 
