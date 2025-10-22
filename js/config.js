d932eea6b003f5d4aeac5442ea5e255fd6f4bded
NB-CyberSec-GSU <nbarro1@gsu.edu>
2025-10-22 11:25:39 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://74w8g63dq0.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
